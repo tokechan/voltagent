76364ae2342fe5c4058ebc76f288eaf8a0ad19b8
toketime <flatoke@gmail.com>
2025-07-17 16:30:20 +0900

    client setting

--- portforio-agent/client/src/App.tsx
+++ portforio-agent/client/src/App.tsx
@@ -1,9 +1,59 @@
 import { Button } from "./components/ui/button";
+import { useState } from "react";
+
+
 
 function App() {
+  const [portfolio, setPortfolio] = useState<string>("");
+  
+  const generatePortfolio = async (qiitaId: string, githubId: string) => {
+    const response = await fetch(
+      "http://localhost:3141/agents/main-agent/text",
+      {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          input: `Qiita ID: ${qiitaId} \n GitHub ID: ${githubId}`,
+          options: {
+            userId: "unique-user-id",
+            conversationId: "unique-conversation-id",
+            contextLimit: 10,
+            temperature: 0.7,
+            maxTokens: 100,
+          },
+        }),
+      }
+    );
+
+    const res = (await response.json()) as {
+      data: { provider: { text: string } };
+    };
+
+    return res.data.provider.text;
+  };
+
+  const handleGeneratePortfolio = async (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+
+    const qiitaId = e.currentTarget.qiitaId.value;
+    const githubId = e.currentTarget.githubId.value;
+
+    const portfolio = await generatePortfolio(qiitaId, githubId);
+    setPortfolio(portfolio);
+  }
   return (
     <div>
-      <Button>Click me</Button>
+      <form onSubmit={handleGeneratePortfolio}>
+        <input type="text" name="qiitaId" placeholder="Qiita ID" />
+        <input type="text" name="githubId" placeholder="GitHub ID" />
+        <Button type="submit">Click me</Button>
+        </form>
+        <div>{portfolio}</div>
     </div>
   );
 }
