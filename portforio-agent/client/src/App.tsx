d596ee146fbdcef0405a6b758d7702661854241e
toketime <flatoke@gmail.com>
2025-07-17 13:55:54 +0900

    add client side

---
+++ portforio-agent/client/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default App;
