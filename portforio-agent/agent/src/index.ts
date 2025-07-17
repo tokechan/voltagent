c7e91f41a2e0fa2f5ca91bad7675223015fc71c1
toketime <flatoke@gmail.com>
2025-07-17 14:24:56 +0900

    add APIkey

--- portforio-agent/agent/src/index.ts
+++ portforio-agent/agent/src/index.ts
@@ -1,13 +1,38 @@
-import { VoltAgent, Agent } from "@voltagent/core";
+import { VoltAgent, Agent, createTool } from "@voltagent/core";
 import { VercelAIProvider } from "@voltagent/vercel-ai";
+
 import { openai } from "@ai-sdk/openai";
+import { z } from "zod";
+
+const getQiitaUserInfo = createTool({
+  name: "getQiitaUserInfo",
+  description: "QittaUserの情報を取得する",
+  parameters: z.object({
+    userId: z.string().describe("QiitaユーザーID"),
+  }),
+  execute: async ({ userId }) => {
+    const accessToken = process.env.QIITA_API_KEY;
+    const response = await fetch(`https://qiita.com/api/v2/users/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const data =  await response.json();
+    return data;
+  },
+});
 
 const agent = new Agent({
-  name: "agent",
-  instructions: "A helpful assistant that answers questions without using tools",
+  name: "qitta-agent",
+  instructions: `ユーザーからQiitaユーザーIDを受け取ったら、ユーザーの情報を取得してください。`,
+  parameters: z.object({
+    userId: z.string().describe("QiitaユーザーID"),
+  }),
   llm: new VercelAIProvider(),
   model: openai("gpt-4o-mini"),
-  tools: [],
+  tools: [
+    getQiitaUserInfo,
+  ],
 });
 
 new VoltAgent({
