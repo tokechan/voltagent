8196c872d036d47c5232ab02934bec00d786e654
toketime <flatoke@gmail.com>
2025-07-17 14:03:51 +0900

    add shadcn

--- portforio-agent/client/src/App.tsx
+++ portforio-agent/client/src/App.tsx
@@ -1,9 +1,9 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div>
-      <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
-        Click me
-      </button>
+      <Button>Click me</Button>
     </div>
   );
 }
